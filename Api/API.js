d58db6e69fce7a070c834a93425eeeb029f9159c
Hagoort, Bart <42860246+LokiTheShadySpirit@users.noreply.github.com>
2023-11-16 09:47:24 +0100

    intermittant push

--- Api/API.js
+++ Api/API.js
@@ -7,6 +7,7 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.get("/", function (req, res) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //dirty fix as discussed
   res.send({
     message: "Hello world from stat api",
   });
